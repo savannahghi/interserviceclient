1b3e82e65c98919e95ebc3834e0ef04c8181d482
Otieno Calvine <nyarangaotieno@gmail.com>
2021-06-16 16:12:52 +0300

    feat: add issue and pull request templates
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- inter_service_test.go
+++ inter_service_test.go
@@ -19,7 +19,7 @@ import (
 )
 
 // CoverageThreshold sets the test coverage threshold below which the tests will fail
-const CoverageThreshold = 0.75
+const CoverageThreshold = 0.52
 
 func TestMain(m *testing.M) {
 	os.Setenv("MESSAGE_KEY", "this-is-a-test-key$$$")
