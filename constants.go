8e83ad66e59902f5e2dafa22a20eed0a8647c290
Otieno Calvine <nyarangaotieno@gmail.com>
2021-07-22 10:18:43 +0300

    chore: remove duplicate constants
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- constants.go
+++ constants.go
@@ -15,21 +15,6 @@ const (
 	// via REST, need to have this file in their root
 	DepsFileName = "deps.yaml"
 
-	// env variable pointing to where this service is running e.g staging, testing, prod
-	Environment = "ENVIRONMENT"
-
-	// running the service under staging
-	StagingEnv = "staging"
-
-	// running the service under demo
-	DemoEnv = "demo"
-
-	// running the service under testing
-	TestingEnv = "testing"
-
-	// running the service under production
-	ProdEnv = "prod"
-
 	// running the service under e2e
 	E2eEnv = "e2e"
 
